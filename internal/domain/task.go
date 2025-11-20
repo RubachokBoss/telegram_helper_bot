e6b92cd383736a8e8c6153ccf1766a5248878af8
GlebDurov <waterskanibal@gmail.com>
2025-11-20 14:42:05 +0300

    domain task struct and interfaces

---
+++ internal/domain/task.go
@@ -0,0 +1,28 @@
+package domain
+
+type Task struct {
+	ID         string `json:"id"`
+	Text       string `json:"text"`
+	OwnerID    string `json:"owner_id"`
+	AssignedID string `json:"assigned_id"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+}
+
+type TaskRepository interface {
+	Create(task *Task) error
+	FindByID(id string) (*Task, error)
+	FindByUserID(userID string) (*[]Task, error)
+	FindByOwnerID(ownerID string) (*[]Task, error)
+	Update(task *Task) error
+	Delete(task *Task) error
+}
+
+type TaskService interface {
+	CreateTask(text, ownerId string) (*Task, error)
+	AssignTask(taskID, userId string) (*Task, error)
+	UnassignTask(taskID string) (*Task, error)
+	ResolveTask(taskID string) error
+	GetUserTasks(userID string) ([]*Task, error)
+	GetOwnerTasks(ownerID string) ([]*Task, error)
+}
